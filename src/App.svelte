71cd9a807e16880c5ac8b07c74616ad3fe1d48f2
coderadu <47945947+coderadu@users.noreply.github.com>
2021-06-23 14:47:37 +0300

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import './css/materialize.min.css'
+  import Counter from './components/Counter.svelte'
+</script>
+
+<main>
+  <div class="container">
+    <h4>Counter</h4>
+    <!-- svelte-ignore missing-declaration -->
+    <Counter />
+  </div>
+</main>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let count=0
+  function add(){
+    count+=1
+    localStorage.setItem('countr-count', count.toString())
+  }
+  function getCount(){
+    count=+localStorage.getItem('countr-count')
+  }
+  function reset(){
+    count=0
+    localStorage.removeItem('countr-count')
+  }
+  getCount()
+</script>
+
+<main>
+  <button class="waves-effect waves-light btn" on:click={add}>Clicked {count} times</button>
+  <br>
+  <button class="waves-effect waves-light btn" on:click={reset}>Reset count</button>
+</main>
